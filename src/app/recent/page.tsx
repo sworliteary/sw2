50f8a2e33442351d5ce8601860ee299e34616e6e
sworliteary <shnovels@gmail.com>
2023-10-11 22:31:21 +0900

    recent のページの metadata を追加

--- src/app/recent/page.tsx
+++ src/app/recent/page.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import { GetRecentWorks } from "../data/data";
 import { WorkInfoList } from "../components/work";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "最近の更新 | Sayonara Voyage",
+  description: "最近の更新のページの一覧です。",
+};
 
 export default async function Recent() {
   const recent = (await GetRecentWorks()).slice(0, 10);
